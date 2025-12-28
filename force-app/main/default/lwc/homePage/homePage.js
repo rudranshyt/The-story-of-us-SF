215d8745f9f2c027a2ce2a0a4ee248c2a447a8da
rudranshyt <90946946+rudranshyt@users.noreply.github.com>
2025-12-28 18:15:38 +0530

    added a home page skeleton

---
+++ force-app/main/default/lwc/homePage/homePage.js
@@ -0,0 +1,17 @@
+import { LightningElement,wire,track,api } from 'lwc';
+import Name from '@salesforce/schema/User.Name';
+import { getRecord } from 'lightning/uiRecordApi';
+import Id from '@salesforce/user/Id';
+
+export default class HomePage extends LightningElement {
+    userName = Name;
+
+    @wire(getRecord, { recordId: Id, fields: [Name] })
+    wiredUser({ error, data }) {
+        if (error) {
+            console.error('Error retrieving user data:', error);
+        } else if (data) {
+            this.userName = data.fields.Name.value;
+        }
+    }
+}
